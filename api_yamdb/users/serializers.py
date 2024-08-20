b6762b2edb0a2403117ad1848bfd0a5a691fd093
Goarik Mkrtchyan <mkrtchyangoarik@gmail.com>
2024-08-20 13:31:16 +0300

    4

--- api_yamdb/users/serializers.py
+++ api_yamdb/users/serializers.py
@@ -16,32 +16,12 @@ class UserSerializer(serializers.ModelSerializer):
             'last_name', 'bio', 'role')
         read_only_fields = ('role',)
 
-    def create(self, validated_data):
-        # Если требуется, вы можете установить роль здесь
-        user = User.objects.create(**validated_data)
-        return user
-
 
 class SignUpSerializer(serializers.ModelSerializer):
-    username = serializers.RegexField(regex=r'^[\w.@+-]+\Z', max_length=150)
 
     class Meta:
         model = User
         fields = ('email', 'username')
-
-    def validate_username(self, value):
-        if len(value) > 150:
-            raise serializers.ValidationError("Username cannot be longer than 150 characters.")
-        if value == 'me':
-            raise serializers.ValidationError("The username 'me' is not allowed.")
-        if User.objects.filter(username=value).exists():
-            raise serializers.ValidationError("Username already exists.")
-        return value
-
-    def validate_email(self, value):
-        if User.objects.filter(email=value).exists():
-            raise serializers.ValidationError("Email already exists.")
-        return value
 
 
 class TokenSerializer(serializers.ModelSerializer):
